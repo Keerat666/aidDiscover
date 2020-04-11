706a7022df61702d9bbdb9286bcd1e7bc937a7db
Keerat666 <gurkeeratsondhi@gmail.com>
2020-04-12 04:32:37 +0530

    added new components to hello-world

--- hello world/src/app/app.module.ts
+++ hello world/src/app/app.module.ts
@@ -1,13 +1,45 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ListingFormComponent } from './listing-form/listing-form.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
+import { MyProfileComponent } from './my-profile/my-profile.component';
+
+
+const appRoutes: Routes = [
+  { path: 'create_post', component: ListingFormComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'dashboard', component: UserDashboardComponent },
+  { path: 'profile', component: MyProfileComponent },
+
+
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    ListingFormComponent,
+    LoginComponent,
+    RegisterComponent,
+    UserDashboardComponent,
+    MyProfileComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule
   ],
   providers: [],
