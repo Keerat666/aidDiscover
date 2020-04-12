8cb21b88e0a1fa778ae087f0f686796dc71ff6af
digitrix08 <arupkumar_sudhir@srmuniv.edu.in>
2020-04-13 00:15:28 +0530

    added cors implementation

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -40,17 +40,19 @@ INSTALLED_APPS = [
     'accounts',
     'posts',
     'rest_framework',
-    'rest_framework.authtoken'
+    'rest_framework.authtoken',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware'
 ]
 
 ROOT_URLCONF = 'backend.urls'
